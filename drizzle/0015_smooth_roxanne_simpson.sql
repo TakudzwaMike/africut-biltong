e9b70baf3dc095c2b8d98829d4e5d4d01629e855
biase-d <tashingachikwanha@gmail.com>
2025-08-26 08:10:52 +0200

    schema: migrations

---
+++ drizzle/0015_smooth_roxanne_simpson.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "product" ADD COLUMN "cta_text" varchar(255);--> statement-breakpoint
+ALTER TABLE "product" ADD COLUMN "cta_link" varchar(255);--> statement-breakpoint
+ALTER TABLE "solution" ADD COLUMN "cta_text" varchar(255);--> statement-breakpoint
+ALTER TABLE "solution" ADD COLUMN "cta_link" varchar(255);
